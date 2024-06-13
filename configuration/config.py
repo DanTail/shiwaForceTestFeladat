d41a14196bb22113666c683ee513cb8d9f29e2e6
DanTail <szabo.daniel977@gmail.com>
2024-06-13 15:44:53 +0200

    add config file and dependencies

---
+++ configuration/config.py
@@ -0,0 +1,8 @@
+class TestData:
+    CHROME_EXECUTABLE_PATH = "drivers/chromedriver.exe"
+    FIREFOX_EXECUTABLE_PATH = "Drivers/geckodriver.exe"
+
+    URL = "https://automationintesting.online/"
+    CONTACTNAME = "Contact Name"
+    CONTACTEMAIL = "contact.email@contact.email"
+    CONTACTPHONE = "06987654321"
